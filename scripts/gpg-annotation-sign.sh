0d3ef9c1691b08b1631e6de95abda27eb8e5304b
gajan@jp.ibm.com <gajan@jp.ibm.com>
2021-08-30 15:21:05 +0900

    Fixed typo

--- scripts/gpg-annotation-sign.sh
+++ scripts/gpg-annotation-sign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 20201 IBM Corporation.
+# Copyright 2021 IBM Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
